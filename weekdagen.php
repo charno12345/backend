3ec978b188740303d257af373841d78a1aa15c6c
charno12345 <36038072+charno12345@users.noreply.github.com>
2018-02-01 14:18:26 +0100

    Add files via upload

---
+++ weekdagen.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <?php
+    $days = array("Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag", "Zondag");
+    
+    foreach ($days as $value) {
+        echo "$value <br> ";
+    }
+?>
+</body>
+</html>
